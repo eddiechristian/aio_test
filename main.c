6af92101c2d202044a5f1ea9d96350f42d8953a1
eddie christian <eddie.christian@ironnetcybersecurity.com>
2017-04-13 12:44:54 -0400

    writev working

--- main.c
+++ main.c
@@ -1,169 +1,259 @@
+/*
+ *  eventfd-aio-test by Davide Libenzi (test app for eventfd hooked into KAIO)
+ *  Copyright (C) 2007  Davide Libenzi
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Davide Libenzi <davidel@xmailserver.org>
+ *
+ */
+
 #define _GNU_SOURCE
-
-#include <unistd.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/param.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <sys/select.h>
-#include <inttypes.h>
 #include <sys/syscall.h>
-#include <linux/aio_abi.h>
+#include <sys/types.h>
+#include <sys/signal.h>
+#include <sys/time.h>
+#include <sys/uio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
 
-int g_max_file_sz = 10000; //10 meg
-int g_wrt_sz = 65536; //10 meg
 
-#define MAX_PACKETS (100*2)
-struct iovec g_staged_packets[MAX_PACKETS];
+/*
+ * This were good at the time of 2.6.21-rc5.mm4 ...
+ */
+#ifndef __NR_eventfd
+#if defined(__x86_64__)
+#define __NR_eventfd 284
+#elif defined(__i386__)
+#define __NR_eventfd 323
+#else
+#error Cannot detect your architecture!
+#endif
+#endif
 
-struct ed_pcap_pkthdr {
-     uint32_t ts_sec;         /* timestamp seconds */
-     uint32_t ts_usec;        /* timestamp microseconds */
-     uint32_t caplen;
-     uint32_t len;
- };
+#define BUILD_BUG_IF(c) ((void) sizeof(char[1 - 2 * !!(c)]))
 
-void usage()
+#define TESTFILE_SIZE (4096 * 5200)
+#define IORTX_SIZE (1024 * 4)
+#define NUM_EVENTS 128
+
+
+typedef unsigned long aio_context_t;
+
+enum {
+	IOCB_CMD_PREAD = 0,
+	IOCB_CMD_PWRITE = 1,
+	IOCB_CMD_FSYNC = 2,
+	IOCB_CMD_FDSYNC = 3,
+	/* These two are experimental.
+	 * IOCB_CMD_PREADX = 4,
+	 * IOCB_CMD_POLL = 5,
+	 */
+	IOCB_CMD_NOOP = 6,
+	IOCB_CMD_PREADV = 7,
+	IOCB_CMD_PWRITEV = 8,
+};
+
+#if defined(__LITTLE_ENDIAN)
+#define PADDED(x,y)	x, y
+#elif defined(__BIG_ENDIAN)
+#define PADDED(x,y)	y, x
+#else
+#error edit for your odd byteorder.
+#endif
+
+#define IOCB_FLAG_RESFD		(1 << 0)
+
+/*
+ * we always use a 64bit off_t when communicating
+ * with userland.  its up to libraries to do the
+ * proper padding and aio_error abstraction
+ */
+struct iocb {
+	/* these are internal to the kernel/libc. */
+	u_int64_t	aio_data;	/* data to be returned in event's data */
+	u_int32_t	PADDED(aio_key, aio_reserved1);
+	/* the kernel sets aio_key to the req # */
+
+	/* common fields */
+	u_int16_t	aio_lio_opcode;	/* see IOCB_CMD_ above */
+	int16_t	aio_reqprio;
+	u_int32_t	aio_fildes;
+
+	u_int64_t	aio_buf;
+	u_int64_t	aio_nbytes;
+	int64_t	aio_offset;
+
+	/* extra parameters */
+	u_int64_t	aio_reserved2;	/* TODO: use this for a (struct sigevent *) */
+
+	u_int32_t	aio_flags;
+	/*
+	 * If different from 0, this is an eventfd to deliver AIO results to
+	 */
+	u_int32_t	aio_resfd;
+}; /* 64 bytes */
+
+struct io_event {
+	u_int64_t           data;           /* the data field from the iocb */
+	u_int64_t           obj;            /* what iocb this event came from */
+	int64_t           res;            /* result code for this event */
+	int64_t           res2;           /* secondary result */
+};
+
+static void asyio_prep_pwritev(struct iocb *iocb, int fd, struct iovec *iov,
+			       int nr_segs, int64_t offset, int afd)
 {
-	fprintf(stderr,
-		"Usage: aio_test [-z maxfilesize default 10M] [-b wrtsize default 65k]\n");
-
-	exit(1);
+	memset(iocb, 0, sizeof(*iocb));
+	iocb->aio_fildes = fd;
+	iocb->aio_lio_opcode = IOCB_CMD_PWRITEV;
+	iocb->aio_reqprio = 0;
+	iocb->aio_buf = (u_int64_t) iov;
+	iocb->aio_nbytes = nr_segs;
+	iocb->aio_offset = offset;
+	iocb->aio_flags = IOCB_FLAG_RESFD;
+	iocb->aio_resfd = afd;
 }
 
-inline int io_setup(unsigned nr, aio_context_t *ctxp)
-{
-	return syscall(__NR_io_setup, nr, ctxp);
+static long io_setup(unsigned nr_reqs, aio_context_t *ctx) {
+	return syscall(__NR_io_setup, nr_reqs, ctx);
 }
 
-inline int io_destroy(aio_context_t ctx)
-{
+static long io_destroy(aio_context_t ctx) {
 	return syscall(__NR_io_destroy, ctx);
 }
 
-inline int io_submit(aio_context_t ctx, long nr,  struct iocb **iocbpp)
-{
-	return syscall(__NR_io_submit, ctx, nr, iocbpp);
+static long io_submit(aio_context_t ctx, long n, struct iocb **paiocb) {
+	return syscall(__NR_io_submit, ctx, n, paiocb);
 }
 
-inline int io_getevents(aio_context_t ctx, long min_nr, long max_nr,
-		struct io_event *events, struct timespec *timeout)
-{
-	return syscall(__NR_io_getevents, ctx, min_nr, max_nr, events, timeout);
+static long io_cancel(aio_context_t ctx, struct iocb *aiocb,
+		      struct io_event *res) {
+	return syscall(__NR_io_cancel, ctx, aiocb, res);
 }
 
-uint64_t create_iocb_array_from_pkt(
-        int fd,
-        unsigned char* pkt,
-        struct ed_pcap_pkthdr* pcap_pkt_hdr,
-        uint64_t start_offset,
-        struct iocb ** pcap_hdr_cb,
-        struct iocb ** pcap_pkt_cb
-      )
-{
-    *pcap_hdr_cb = malloc(sizeof(struct iocb));
-    memset(*pcap_hdr_cb, 0, sizeof(struct iocb));
-    *pcap_pkt_cb = malloc(sizeof(struct iocb));
-    memset(*pcap_pkt_cb, 0, sizeof(struct iocb));
-
-    g_staged_packets[3].iov_len = pcap_pkt_hdr->len;
-
-    (*pcap_hdr_cb)->aio_fildes = fd;
-    (*pcap_hdr_cb)->aio_lio_opcode = IOCB_CMD_PWRITE; //IOCB_CMD_PWRITEV
-    (*pcap_hdr_cb)->aio_buf = pcap_pkt_hdr;
-    (*pcap_hdr_cb)->aio_offset = start_offset;
-    (*pcap_hdr_cb)->aio_nbytes = 16;
-
-
-    (*pcap_pkt_cb)->aio_fildes = fd;
-    (*pcap_pkt_cb)->aio_lio_opcode = IOCB_CMD_PWRITE;//IOCB_CMD_PWRITEV
-    (*pcap_pkt_cb)->aio_buf = (uint64_t)pkt;
-    (*pcap_pkt_cb)->aio_offset = start_offset+16;
-    (*pcap_pkt_cb)->aio_nbytes = pcap_pkt_hdr->len;
-    return start_offset + 16 + pcap_pkt_hdr->len;
+static long io_getevents(aio_context_t ctx, long min_nr, long nr,
+			 struct io_event *events, struct timespec *tmo) {
+	return syscall(__NR_io_getevents, ctx, min_nr, nr, events, tmo);
 }
 
-int main(int argc, char *argv[])
-{
-  extern char *optarg;
-  extern int optind, opterr, optopt;
-  int c;
-  int ret;
-  aio_context_t  myctx;
-  struct iocb cb;
-  struct iocb *cbs[1000];
-  struct io_event events[1000];
-  char data[4096];
-  struct ed_pcap_pkthdr pcap_pkt_hdr;
-  pcap_pkt_hdr.ts_sec = 0;
-  pcap_pkt_hdr.ts_usec = 0;
-  pcap_pkt_hdr.caplen = 1345;
-  pcap_pkt_hdr.len = 1345;
-  memset(data,3,4096);
-  memset(g_staged_packets,0,sizeof(g_staged_packets));
-
-  while ((c = getopt(argc, argv, "hz:b:")) != -1) {
-    char *endp;
-
-    switch (c) {
-      case 'z':	/* alignment of data buffer */
-        g_max_file_sz = strtol(optarg, &endp, 0);
-        break;
-      case 'b':	/* alignment of data buffer */
-        g_wrt_sz = strtol(optarg, &endp, 0);
-        break;
-      case 'h':
-       usage();
-       break;
-    }
-  }
-
-  printf("g_max_file_sz %d\n", g_max_file_sz);
-  printf("g_wrt_sz %d\n", g_wrt_sz);
-
-  int fd = open("foo", O_WRONLY|O_CREAT, 0777);
-  if (fd) {
-       int mode = 0;
-       ret = fallocate(fd,mode,0,g_max_file_sz);
-       if (ret < 0) {
-          perror("fallocate failed");
-          return -1;
-       }
-       memset(&myctx, 0, sizeof(myctx));
-       ret = io_setup(1, &myctx);
-       if (ret < 0) {
-         perror("io_setup error");
-         return -1;
-       }
-
-      uint64_t offset = create_iocb_array_from_pkt(fd, data, &pcap_pkt_hdr, 0, &cbs[0],&cbs[1]);
-
-       int n = 2;
-       ret = io_submit(myctx, n, &cbs[0]);
-       if (ret != n) {
-         if (ret < 0)
-           perror("io_submit error");
-         else
-           fprintf(stderr, "could not sumbit IOs");
-         return  -1;
-       }
-       printf("io_submit ret %d\n", ret);
-
-       /* get the reply */
-       ret = io_getevents(myctx, 1, 2, events, NULL);
-       printf("io_getevents ret %d\n", ret);
-       printf("events[0].res %d events[1].res %d \n", events[0].res,events[1].res);
-       printf("events[0].res2  %d events[1].res2  %d \n", events[0].res2,events[1].res2);
-       ret = io_destroy(myctx);
-       if (ret < 0) {
-         perror("io_destroy error");
-         return -1;
-       }
-
-  }
-  return 0;
-  close(fd);
+static int eventfd(int count) {
+	return syscall(__NR_eventfd, count);
 }
+
+
+static long test_writev(aio_context_t ctx, int fd, int iovcnt, int afd) {
+	long i, r, j;
+	u_int64_t eval;
+	struct iocb **piocb;
+	struct iocb *iocb;
+	struct iovec *iovec;
+	struct timespec tmo;
+	static struct io_event events[NUM_EVENTS];
+	static char buf[512];
+
+	for (i = 0; i < 512; i++)
+		buf[i] = i & 0xff;
+
+	snprintf(buf, sizeof(buf), "Hello Eddie!");
+	buf[0] = 'H';
+	buf[1] = 'e';
+	buf[2] = 'l';
+	buf[3] = 'l';
+	buf[4] = 'o';
+	buf[5] = '\0';
+
+	fprintf(stderr, "buffer before=%s\n", buf);
+
+	iocb = malloc( sizeof(struct iocb));
+	piocb = malloc(sizeof(struct iocb *));
+	iovec = malloc(iovcnt * sizeof(struct iovec));
+	if (!iocb || !piocb || !iovec) {
+		perror("iocb alloc");
+		return -1;
+	}
+	for (i = 0; i < iovcnt; i++) {
+		iovec[i].iov_base = buf;
+		iovec[i].iov_len = sizeof(buf);
+	}
+        piocb[0] = &iocb[0];
+        asyio_prep_pwritev(&iocb[0], fd, iovec, iovcnt, 0, afd);
+        iocb[0].aio_data = (u_int64_t) 1;
+
+	fprintf(stdout, "submitting write requests (%ld) ...\n", 1);
+	if ((r = io_submit(ctx, 1, piocb)) <= 0) {
+		perror("io_submit");
+		return -1;
+	}
+	fprintf(stdout, "submitted %ld requests\n", r);
+	
+	
+	r = io_getevents(ctx, 1, NUM_EVENTS, events, &tmo);
+	if (r > 0) {
+		for (j = 0; j < r; j++) {
+			fprintf(stderr, "event[%d]: .res=%d .res2=%d\n", j, events[j].res, events[j].res2);
+		}
+	}
+	
+	free(iocb);
+	free(piocb);
+
+	fprintf(stderr, "buffer after=%s\n", buf);
+
+	return 1;
+}
+
+int main(int ac, char **av) {
+	int afd, fd;
+	aio_context_t ctx = 0;
+	char const *testfn = "/tmp/eventfd-aio-test.data";
+
+	BUILD_BUG_IF(sizeof(struct iocb) != 64);
+
+	fprintf(stdout, "creating an eventfd ...\n");
+	if ((afd = eventfd(0)) == -1) {
+		perror("eventfd");
+		return 2;
+	}
+	fprintf(stdout, "done! eventfd = %d\n", afd);
+	if (io_setup(TESTFILE_SIZE / IORTX_SIZE + 256, &ctx)) {
+		perror("io_setup");
+		return 3;
+	}
+	if ((fd = open(testfn, O_RDWR | O_CREAT, 0644)) == -1) {
+		perror(testfn);
+		return 4;
+	}
+	ftruncate(fd, TESTFILE_SIZE);
+
+	fcntl(afd, F_SETFL, fcntl(afd, F_GETFL, 0) | O_NONBLOCK);
+
+	test_writev(ctx, fd, 128, afd);
+	
+        io_destroy(ctx);
+	close(fd);
+	close(afd);
+	//remove(testfn);
+
+	return 0;
+}
+
